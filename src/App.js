a56265b8119c93a5b8a8b8bce6f054a729230336
VNDDNV <142884734+VNDDNV@users.noreply.github.com>
2023-08-29 20:48:44 +1000

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { FirebaseConfig } from "./config/Config";
+import { initializeApp } from "firebase/app";
+
+import "react-bootstrap/Container"
+import "react-bootstrap/Navbar"
+import "react-bootstrap/Nav"
+import './app.css'
+import Container from "react-bootstrap/Container"
+
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from "react-bootstrap";
 
 function App() {
+  const FBapp = initializeApp(FirebaseConfig)
   return (
     <div className="App">
+    <Navbar>
+      <Container>
+        
+      </Container>
+
+    </Navbar>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
