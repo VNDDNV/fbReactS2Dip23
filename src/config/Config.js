cd56aa9e7b8f42fad132d70b878e6e4546d1eae5
VNDDNV <142884734+VNDDNV@users.noreply.github.com>
2023-08-29 18:59:20 +1000

    message

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyDhJI6pK_N7H7IkgIJXtSY2NkL-6prKzso",
+    authDomain: "classproject2-54f6f.firebaseapp.com",
+    projectId: "classproject2-54f6f",
+    storageBucket: "classproject2-54f6f.appspot.com",
+    messagingSenderId: "353009519940",
+    appId: "1:353009519940:web:82b55aed7bf541aa2b5b11"    
+}
